56ac1f37608786bdcc68c08be51779eb735fa40f
kroshkamedved <kroshkamedved@gmail.com>
2024-10-25 20:33:37 +0300

    bug: without import

--- taskTest/jsrc/main/java/com/taskTest/ApiHandler.java
+++ taskTest/jsrc/main/java/com/taskTest/ApiHandler.java
@@ -3,10 +3,8 @@ package com.taskTest;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.RetentionSetting;
-
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import java.util.HashMap;
 import java.util.Map;
